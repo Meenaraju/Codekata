0cb79df0b32c08cd8426a3dd44f3fe439010a1a5
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-17 09:39:50 +0530

    Update findingpositivenegative&zero.java

--- Beginner/findingpositivenegative&zero.java
+++ Beginner/findingpositivenegative&zero.java
@@ -7,18 +7,18 @@ class integer
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	
-Scanner sc = new Scanner(System.in);
+               Scanner sc = new Scanner(System.in);
 		String x = sc.nextLine();
 		try{
 		int n = Integer.parseInt(x);	
 		if(n>=1 && n<=100000){
-			System.out.println("positive");
+			System.out.println("Positive");
 		}
 		else if(n<=-1){
-			System.out.println("negative");
+			System.out.println("Negative");
 		}
 		else if(n==0){
-			System.out.println("zero");
+			System.out.println("Zero");
 		}
 		else{
 			System.out.println("the range exceed");
