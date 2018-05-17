d6475b718ddd193bbdbbf0a36798960e0d5c4bc8
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-17 13:53:52 +0530

    Update findingintegerornot.java

--- Beginner/findingintegerornot.java
+++ Beginner/findingintegerornot.java
@@ -6,15 +6,21 @@ class integerornot
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-  Scanner sc = new Scanner(System.in);
+               Scanner sc = new Scanner(System.in);
 		String x = sc.nextLine();
 		try{
+			if(n>=1 && n<=100000){
+
 			int n = Integer.parseInt(x);
 			if(n%2==0){
 				System.out.println("Even");
 			}
 			else{
-				System.out.println("odd");
+				System.out.println("Odd");
+			}
+			}
+			else{
+				System.out.println("range exceed");
 			}
 		}
 		catch(NumberFormatException nFE)	{
