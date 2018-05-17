e3e46b80994c91edea7d2ab179bf9abb9f149841
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-17 09:46:01 +0530

    Update characterisvowelorconsonant.java

--- Beginner/characterisvowelorconsonant.java
+++ Beginner/characterisvowelorconsonant.java
@@ -10,11 +10,11 @@ class vowelornot
 	            	char ch=sc.next( ).charAt(0);	 
 	
 	    	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='i'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
-	    		System.out.println("vowels");
+	    		System.out.println("Vowels");
 	    	}
 	    	else {
 	    	if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
-	    		System.out.println("consonant");
+	    		System.out.println("Consonant");
 	    	
 	    
 		else
