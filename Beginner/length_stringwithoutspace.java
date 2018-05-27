c1b4b08f9b313c410e9afeab9bf385225369d1bb
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-27 12:02:36 +0530

    Create length_stringwithoutspace.java

---
+++ Beginner/length_stringwithoutspace.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+			
+		     System.out.println(s.replace(" ","").length());
+			
+	}
+}
