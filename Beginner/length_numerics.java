099389790e3a685a969d0a500d93205a42e86ba3
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-27 13:30:11 +0530

    Create length_numerics.java

---
+++ Beginner/length_numerics.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int n=1;
+			if(n<=1000){
+		    System.out.println(s.replaceAll("\\d","").length());
+			}
+		else{
+			System.out.println("invalid input");
+		}
+			
+	}
+}
