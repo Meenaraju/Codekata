34f8bbeb8b3ec1d42cb10d35c3ca997a58c4a897
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-27 14:05:04 +0530

    Create length_splchar.java

---
+++ Beginner/length_splchar.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int n=0;
+			if(n<=1000){
+		    String[] s1 = s.split("[\\!#%^*@&?$+-]+");
+		    System.out.println(s1.length);
+			}
+		else{
+			System.out.println("invalid input");
+		}
+			
+	}
+}
