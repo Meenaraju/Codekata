0b2d992a7bde35ef0eec365b151884e35ced4bf6
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-28 13:19:32 +0530

    Create print_ktimes.java

---
+++ Beginner/print_ktimes.java
@@ -0,0 +1,30 @@
+
+	   
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String s1 = sc.next();
+		try{
+			int n = Integer.parseInt(s1);
+			for(int i=0;i<n;i++){
+	
+			System.out.println(s);
+			}	
+		}
+		catch(NumberFormatException nef){
+			System.out.println("invalid input");
+		}	
+			
+			
+	}
+}
+	
