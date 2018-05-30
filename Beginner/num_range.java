c50b739586bb52279f4e10f62fb589ac61f5fa23
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-30 14:29:48 +0530

    Create num_range.java

---
+++ Beginner/num_range.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		try{
+			
+			int n = Integer.parseInt(s);
+			if(n>=1 && n<=10)
+			{
+			System.out.println("Yes");
+		}
+		else{
+			System.out.println("No");
+		}
+		}
+		catch(NumberFormatException nef){
+			System.out.println("invalid input");
+		}
+
+	}
+}
