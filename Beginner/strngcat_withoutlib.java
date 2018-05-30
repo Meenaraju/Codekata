f53bda1daa3623aaa158826075d325e2a1803c4a
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-30 13:38:37 +0530

    Create strngcat_withoutlib.java

---
+++ Beginner/strngcat_withoutlib.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ class Ideone{
+   public static void main(String arg[])throws Exception {
+      try{
+          int i;
+          Scanner sc = new Scanner(System.in);
+          String s=sc.next();
+          String s1=sc.next();
+          String[] ss=new String[2];
+          for(i=0;i<2;i++){
+          if((s.length()<0||s.length()>1000)&&(s1.length()<0||s1.length()>1000))
+                System.out.println(" invalid value");
+          else{
+          	
+                ss[0]=s;
+                ss[1]=s1;
+                Arrays.sort(ss);
+            }
+        }
+        System.out.println(ss[0]+""+ss[1]);
+      }
+      
+     catch(Exception e){
+            System.out.println("enter valid value");
+      }
+    }
+}
