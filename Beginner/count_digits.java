6b5a079243dd903c7225bc68776dceeb9ce87105
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-31 14:38:03 +0530

    Create count_digits.java

---
+++ Beginner/count_digits.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+              Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		try{
+			int n = Integer.parseInt(s);
+			for(int i=1;i<=s.length();i++){
+				
+			}
+			System.out.println(+s.length());
+		}
+		catch(NumberFormatException nef){
+			System.out.println("invalid input");
+		}
+	}
+}
