272534d84bcb902193a25c41efb711bc010a7fe6
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-05-31 12:44:32 +0530

    Update add_array.java

--- Beginner/add_array.java
+++ Beginner/add_array.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+	import java.util.Arrays;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -11,18 +11,18 @@ class Ideone
 		
 		Scanner sc = new Scanner(System.in);
 		try{
-	              int a[] = new int[2];
-		           for(int i=0;i<2;i++)
-			{
-			 
-			 a[i] = sc.nextInt();
-			 
-			}
+			int n=2;
+			int i=0;
+	                         int a[] = new int[2];
+			 if(n==2){
+			 a[0] = sc.nextInt();
+			 a[1] = sc.nextInt();
 			int c,b;
 			c = a[0];
 			b = a[1];
-			int sum = Math.addExact(c,b);
-      System.out.println(sum);
+		            int sum = Math.addExact(c,b);
+                                    System.out.println(sum);
+			 }
 			
 	
 		}
