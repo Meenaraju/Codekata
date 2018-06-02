8e4d2d63c359f80651a03d39d19738da1d4e5a38
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-02 21:33:02 +0530

    Create print_withoutsemicolon.java

---
+++ Beginner/print_withoutsemicolon.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    String s = sc.next();
+	    if(System.out.printf(s)!=null){}
+	    
+	}
+}
