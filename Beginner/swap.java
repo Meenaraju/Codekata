41e6534d7d9061803831b25e7f4326ac4a868a7b
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-02 13:22:15 +0530

    Create swap.java

---
+++ Beginner/swap.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String s1 = sc.next();
+		try{
+			int n = Integer.parseInt(s);
+			int n1 = Integer.parseInt(s1);
+			if(n<=100000 && n<=100000){
+				int temp = n;
+				n = n1;
+				n1 = temp;
+			   System.out.println(n+" "+n1);	
+			}
+			else{
+		            System.out.println("size exceeds");
+			}
+			
+		}
+		catch(NumberFormatException nef){
+			System.out.println("invalid");
+		}
+	}
+}
