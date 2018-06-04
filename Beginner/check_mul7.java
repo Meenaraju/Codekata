9680247b4f28dc5ee948e6f9277f2780f35fd83c
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-04 12:17:14 +0530

    Create check_mul7.java

---
+++ Beginner/check_mul7.java
@@ -0,0 +1,31 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc = new Scanner(System.in);
+	             String s = sc.nextLine();
+	     try{
+	     	int n = Integer.parseInt(s);
+	     	
+	     		if(n%7==0){
+	     		System.out.println("yes");
+	     		}
+	     	
+	     	
+	     	else{
+	     		System.out.println("no");
+	     	}
+	     
+	     	
+	     }
+	     catch(NumberFormatException nef){
+	     	System.out.println("invalid input");
+	     }
+	}
+}
