c73274f00080704b90499604b5611c7ebab95a97
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-04 20:21:43 +0530

    Create num_printodd.java

---
+++ Beginner/num_printodd.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     Scanner sc = new Scanner(System.in);
+	     String s = sc.next();
+	     try{
+	     	int n = Integer.parseInt(s);
+	     	if(n<=100000){
+	     	char[] a = s.toCharArray();
+	     	for(int i=0;i<s.length();i++){
+	     		if(a[i]%2!=0){
+	     		             System.out.print(a[i]+" ");
+	     		}
+	     		continue;
+	     	}
+	     	}
+	     	else{
+	     		System.out.println("size exceeds");
+	     	}
+	     	
+	     	
+	     }
+	     catch(NumberFormatException nef){
+	     	System.out.println("invalid input");
+	     }
+	}
+
+}
