6a443951a1d679c4d7cb8546b3568557f1117d46
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-04 12:46:58 +0530

    Create diff_oddeven.java

---
+++ Beginner/diff_oddeven.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String s1 = sc.next();
+		try{
+			int n = Integer.parseInt(s);
+			int n1 = Integer.parseInt(s1);
+			int n2 = n-n1;
+			int size = Math.abs(n2);
+			if(size%2==0){
+				System.out.println("yes");
+			}
+			else{
+				System.out.println("no");
+			}
+		}
+		catch(NumberFormatException nef){
+			System.out.println("invalid");
+		}
+	}
+}
