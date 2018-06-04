324ece11858e126e09949190aa1eef26e79b5246
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-04 11:51:42 +0530

    Create 2prime.java

---
+++ Beginner/2prime.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc = new Scanner(System.in);
+	             String s = sc.nextLine();
+	     try{
+	     	int n = Integer.parseInt(s);
+	     	int b=1;
+	     	int c= 0;
+	     	while(b<=n){
+	     		if(n%b==0)
+	     		c=c+1;
+	     		b++;
+	     	}
+	     	if(c==2){
+	     		System.out.println("yes");
+	     	}
+	     	else{
+	     		System.out.println("no");
+	     	}
+	     	
+	     }
+	     catch(NumberFormatException nef){
+	     	System.out.println("invalid input");
+	     }
+	}
+}
