e4e841b046a9d5f1e5f8f252ee761d89759034fa
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-04 19:26:19 +0530

    Update replacestr_mdltosplchr.java

--- Beginner/replacestr_mdltosplchr.java
+++ Beginner/replacestr_mdltosplchr.java
@@ -4,25 +4,34 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 import java.util.Scanner;
-
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-                        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
-		try{
-		        int n = s.length();
-		        double d = Math.ceil(n/2);
-		        int c = (int)d;
-		        char[] s1 = s.toCharArray();
-                                 s1[c] = '*';
-                                 s = String.valueOf(s1);
-                                 System.out.println(s);
-		}
-		catch(Exception nef){
-			System.out.println("invalid");
-		}
+		int n = s.length();
 		
+		double d = Math.ceil(n/2);
+		int c = (int)d;
+		
+			char[] s1 = s.toCharArray();
+			s1[c]='*';
+			if(n%2!=0){
+			s = String.valueOf(s1);
+			System.out.println(s);
+		}
+		else{
+		     
+		           s1[--c]='*';
+		           
+			s = String.valueOf(s1);
+			System.out.println(s);     
+		}
 	}
 }
+
+
+
+		
+	
