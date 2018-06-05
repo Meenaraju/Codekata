72c8cdecff22853b05a909e723a3950f33c9dbe7
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-05 19:48:01 +0530

    Create ascii.java

---
+++ Beginner/ascii.java
@@ -0,0 +1,16 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		char s = sc.next().charAt(0);
+		int ascii = (int)s;
+		System.out.println(ascii);
+	}
+}
