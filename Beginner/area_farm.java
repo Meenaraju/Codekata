a4d23119f71348c4b3da19309c978e8b5fa07652
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-05 18:28:05 +0530

    Create area_farm.java

---
+++ Beginner/area_farm.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+	       String s = sc.next();
+	       String s1 = sc.next();
+	       try{
+	       	double n = Double.parseDouble(s);
+	       	double n1 = Double.parseDouble(s1);
+	       	double n2 = n*n1;
+	       	System.out.printf("%.6g%n",n2);
+	       	
+	       }
+	       catch(NumberFormatException nef){
+	       	System.out.println("invalid");
+	       }
+	}
+}
