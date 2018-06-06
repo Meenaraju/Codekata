a9153d9937dc89152e8b4f969fc2bc99b2e2b6f8
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-06 13:53:57 +0530

    Create lcm.java

---
+++ Beginner/lcm.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	     Scanner sc = new Scanner(System.in);
+	     String s = sc.next();
+	     String s1 = sc.next();
+	     try{
+	     	int gcd=1;
+	     	int n = Integer.parseInt(s);
+	     	int n1 = Integer.parseInt(s1);
+	           	for(int i=1;i<=n && i<=n1;i++){
+	           		if(n%i==0 && n1%i==0){
+	           			gcd = i;
+	           		}
+	           		
+	           	}
+	           	int lcm = (n*n1)/gcd;
+	       System.out.print(lcm);
+	 
+	     	
+	     }
+		
+		catch(NumberFormatException rte){
+			System.out.println("invalid");
+		}
+
+	}
+}
