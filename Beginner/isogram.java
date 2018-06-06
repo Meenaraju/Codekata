d2bcdfa78b9f69afc350b89107189502a1a985b7
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-06 13:17:15 +0530

    Create isogram.java

---
+++ Beginner/isogram.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		try{
+	     Scanner sc = new Scanner(System.in);
+	     String str = sc.nextLine();
+	     String[] a = str.split("");
+	     List<String> l = Arrays.asList(a);
+	     Set<String> s = new HashSet(l);
+	     if(str.length()<=1000){
+	     if(str.length()==s.size()){
+	     	System.out.println("yes");
+	     }
+	     else{
+	     	System.out.println("no");
+	     }
+	     }
+	     else{
+	     	System.out.println("invalid");
+	     }
+	     	
+	     
+		}
+		catch(RuntimeException rte){
+			System.out.println("invalid");
+		}
+
+	}
+}
