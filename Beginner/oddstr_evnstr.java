10d45c8c880cabe4cef7f412e5a388b2eda47845
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-06 12:30:20 +0530

    Create oddstr_evnstr.java

---
+++ Beginner/oddstr_evnstr.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	try{
+	     Scanner sc = new Scanner(System.in);
+	     String g=" ";
+	     String h=" ";
+	     String a = sc.nextLine();
+	     for(int i=0;i<a.length();i++){
+	     	if(i%2==0){
+	     		 g += a.charAt(i);
+	     	}
+	     	else{
+	     		 h += a.charAt(i);
+	     	}
+	     	
+	     }
+	     System.out.println(g+""+h);
+		}
+		catch(RuntimeException rte){
+			System.out.println("invalid");
+		}
+	}
+}
