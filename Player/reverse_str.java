abed5a311ad22f63542fc15b3c900e5cfa878c3b
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-07 20:31:43 +0530

    Update reverse_str.java

--- Player/reverse_str.java
+++ Player/reverse_str.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -11,10 +10,15 @@ class Ideone
 		String r=" ";
 		try{
 			String s = sc.nextLine();
+			if(s.length()<=100000){
 			for(int i=s.length()-1;i>=0;--i){
 				r +=s.charAt(i);
 			}
 			System.out.print(r);
+			}
+			else{
+				System.out.println("size exceds");
+			}
 		}
 		catch(RuntimeException nef){
 			System.out.println("invalid");
