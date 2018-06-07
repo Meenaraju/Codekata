f1fe96fb568bafa0d6fa723de69fae897278ae6c
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-07 19:56:04 +0530

    Create median.java

---
+++ Beginner/median.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		try{
+			
+			int n = Integer.parseInt(s);
+			if(n>=1 && n<=1000)
+			{
+			int a[] = new int[n];
+			for(int i=0;i<n;i++)
+			{
+				a[i]=sc.nextInt();
+			}
+			
+			Arrays.sort(a);
+			//System.out.println(Arrays.toString(a));
+			double c = Math.ceil((n/2));
+			int d = (int)c;
+			System.out.println(a[d]);
+		}
+		else{
+			System.out.println("the range exceed");
+		}
+			
+			
+		}catch(NumberFormatException nef){
+			System.out.println("invalid input");
+		}
+
+	}
+}
