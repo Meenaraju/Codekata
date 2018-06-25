5100a0037cafeef8b7779584c0c515893db2d361
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-26 00:29:43 +0530

    Update encode_str.java

--- Player/encode_str.java
+++ Player/encode_str.java
@@ -7,15 +7,29 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		String s1 = sc.nextLine();
+		String s1 = sc.next();
+		StringBuffer s = new StringBuffer(s1);
 		char[] c = s1.toCharArray();
 		int n=s1.length();
 		try{
 			if(n>=1 && n<=100000){
 			     for(int i=0;i<n;i++){
-			     	c[i] = (char)(c[i]+3);
+			     	if(c[i]=='x')
+			     	    System.out.print("a");
+			     	else if(c[i]=='y')
+			     	    System.out.print("b");
+			     	else if(c[i]=='z')
+			     	    System.out.print("c");
+			     	else if(c[i]=='X')
+			     	    System.out.print("A");
+			           else if(c[i]=='Y')
+			     	    System.out.print("B");
+			     	else if(c[i]=='Z')
+			     	    System.out.print("C");
+			     	else
+			     	System.out.print((char)(c[i]+3));
 			     }
-			     System.out.print(c);
+			     
 			}
 			else{
 				System.out.println("invalid");
