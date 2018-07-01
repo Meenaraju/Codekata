a202fb5fcf55494174a8a95bfcf568fdaa942be7
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-07-01 13:15:27 +0530

    Update 3pts_sameline.java

--- Player/3pts_sameline.java
+++ Player/3pts_sameline.java
@@ -1,4 +1,4 @@
-import java.util.*;
+	import java.util.*;
 import java.lang.*;
 import java.io.*;
 class Ideone
@@ -6,35 +6,33 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		String[] s1 = new String[2];
-		String[] s2 = new String[2];
-		String[] s3 = new String[2];
-		for(int i=0;i<2;i++){
+		String[] s1 = new String[3];
+		String[] s2 = new String[3];
+		for(int i=0;i<3;i++){
 			s1[i]=sc.next();
 		}
-		for(int i=0;i<2;i++){
+		for(int i=0;i<3;i++){
 			s2[i]=sc.next();
-		}
-		for(int i=0;i<2;i++){
-			s3[i]=sc.next();
 		}
 		try{
 		int[] a = new int[s1.length];
 		int[] b =  new int[s2.length];
-		int[] c = new int[s3.length];
 		int count=0;
-		for(int i=0;i<2;i++){
+		for(int i=0;i<3;i++){
+			
 			a[i]=Integer.parseInt(s1[i]);
 		}
-		for(int i=0;i<2;i++){
+		
+		for(int i=0;i<3;i++){
 			b[i]=Integer.parseInt(s2[i]);
 		}
+		
 		for(int i=0;i<2;i++){
-			c[i]=Integer.parseInt(s3[i]);
-		}
-		for(int i=0;i<2;i++){
-			if(a[i]==b[i] && b[i]==c[i] && c[i]==a[i]){
-			count++;
+			
+			if(a[i]==a[i+1] || b[i]==b[i+1])
+			{
+			
+			count=1;
 			
 			}
 		}
