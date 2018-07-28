3a3f66df06aff4352262ec6851bd0f19a5dbdbdc
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-07-28 20:11:29 +0530

    Update mini_cost.java

--- Pro/mini_cost.java
+++ Pro/mini_cost.java
@@ -10,6 +10,7 @@ class Ideone
 	  String s2 = sc.next();
 	  int count=0;
 	  int max=0;
+	  
 	  if(s1.length()>s2.length()){
 	  	max=s1.length();
 	  }
@@ -19,11 +20,18 @@ class Ideone
 	  	if(s1.length()>=1&&s2.length()>=1){
 	  		if(s1.length()<=1000 && s2.length()<=1000){
 	  		for(int j=0;j<max;j++){
+	  			
+	  			 if(s2.length()==1 ){
+	  				if(s1.charAt(j)==s2.charAt(0))
+	  				count++;
+	  			}
+	  			else{
 	  			if(s1.length()==j || s2.length()==j){
 	  				break;
 	  			}
 	  			else if(s1.charAt(j)==s2.charAt(j)){
 	  				count++;
+	  			}
 	  			}
 	  		}
 	  		if(count>0){
@@ -36,6 +44,6 @@ class Ideone
 	  		}
 	  	}
 	  	
-	  
+	 
 	}
 }
