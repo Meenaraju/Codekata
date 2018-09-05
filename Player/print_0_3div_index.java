e8f8cc90de5cfa9ce730d432a8823ce140b5ed57
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-09-05 23:00:06 +0530

    Create print_0_3div_index.java

---
+++ Player/print_0_3div_index.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String g = sc.nextLine();
+		if(g.length()==0){
+			System.out.println("invalid");
+		}
+		else{
+		char[] ch = g.toCharArray();
+		System.out.print(ch[0]);
+		for(int i=1;i<ch.length;i++){
+			if(i%3==0){
+				System.out.print(ch[i]);
+			}
+		}
+		}
+	}
+}
